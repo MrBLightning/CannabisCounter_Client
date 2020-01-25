b1c2b91b89a553325e0d56e30148b1786da41fef
Barak <mr.lightning@gmail.com>
2020-01-25 18:09:02 +0200

    config.js

--- src/shared/config.js
+++ src/shared/config.js
@@ -3,8 +3,8 @@ export default {
     VERSION: process.env.REACT_APP_APP_VERSION,
     BASENAME: '/',
     API_URL: process.env.REACT_APP_API_URL,
-    IMAGE_URL: "https://client.algoretail.co.il/media/images",
-    PRODUCT_URL: "https://client.algoretail.co.il/media/images/products",
+    IMAGE_URL: "https://cannabis-counter-server.herokuapp.com/media/images",
+    PRODUCT_URL: "https://cannabis-counter-server.herokuapp.com/media/images/products",
     IMAGE_CACHE_VERSION: process.env.REACT_APP_IMAGE_CACHE_VERSION || "v1",
     PUBLIC_URL: process.env.PUBLIC_URL,
 }
